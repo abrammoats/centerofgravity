ab73be9441ef9c71774966b7fad2ea26dea74acf
abram <abram@abrams-mbp.myfiosgateway.com>
2021-10-15 11:39:48 -0400

    Did a bunch of stuff in here, sure hope it works

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ from setuptools import setup
 
 setup(name='centerofgravity',
       version='1.0.0',
-      description='Automated Center of Gravity Mappying',
-      url='http://github.com/smutchler/src',
+      description='Automated Center of Gravity Mapping',
+      url='https://github.com/abrammoats/centerofgravity',
+      download_url = 'https://github.com/abrammoats/centerofgravity/archive/refs/tags/1.0.0.tar.gz',
       author='Scott Mutchler, Abram Moats',
       author_email='smutchler@trilabyte.com, abramdanielmoats@gmail.com',
       license='GPLv3',
       packages=['centerofgravity'],
+      keywords = ['CENTROID', 'MAPPING', 'VISUALIZATION', 'CLUSTERING'],
       install_requires=[
         'pandas',
         'scikit-learn',
@@ -22,6 +24,13 @@ setup(name='centerofgravity',
         'json',
         'math',
         'folium'
+      ],
+      classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GPLv3',   # Again, pick a license
+    'Programming Language :: Python :: 3'      #Specify which pyhton versions that you want to support
       ],
       zip_safe=False)
 
